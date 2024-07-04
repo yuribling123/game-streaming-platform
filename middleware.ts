5422a9aebc9bc163d1a8cbe639f98491dc2ce4ac
milktea <yuribling123@gmail.com>
2024-07-03 21:03:38 -0700

    :rocket: Web Hook (with Clerk)

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,13 @@
 import { clerkMiddleware,createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']);
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)','/api/webhooks(.*)']);
 
 export default clerkMiddleware((auth, request) => {
   if(!isPublicRoute(request)) {
     auth().protect();
   }
+
+
 });
 
 export const config = {
