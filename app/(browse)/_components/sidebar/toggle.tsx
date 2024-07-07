cf2bf0f7c01c7530c460295f920de2bce9f1f784
milktea <yuribling123@gmail.com>
2024-07-07 01:19:50 -0700

    :bug: Hydration Error

--- app/(browse)/_components/sidebar/toggle.tsx
+++ app/(browse)/_components/sidebar/toggle.tsx
@@ -4,6 +4,7 @@ import { useSidebar } from "@/store/use-sidebar";
 import { Button } from "@/components/ui/button";
 import { ArrowLeftFromLine, ArrowRightFromLine } from "lucide-react";
 import { Hint } from "@/components/ui/hint";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export const Toggle = () => {
     const {
@@ -33,3 +34,15 @@ export const Toggle = () => {
     )
         ;
 };
+
+// for hydration error
+export const ToggleSkeleton = () => {
+    return (
+      <div className="p-3 pl-6 mb-2 hidden lg:flex items-center justify-between w-full">
+        <Skeleton className="h-6 w-[100px]" />
+        <Skeleton className="h-6 w-6" />
+      </div>
+    );
+  };
+  
+  
--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -1,6 +1,11 @@
+/* container for other components */
 "use client";
 import { useSidebar } from "@/store/use-sidebar";
 import { cn } from "@/lib/utils";
+import { useEffect, useState } from "react";
+import { useIsClient } from "usehooks-ts";
+import { ToggleSkeleton } from "./toggle";
+import { RecommendedSkeleton } from "./recommended";
 
 // container for other components 
 interface WrapperProps {
@@ -13,6 +18,18 @@ export const Wrapper = ({
 }: WrapperProps) => {
     //  extract the collapse state property from the sidebar's state
     const { collapsed } = useSidebar((state) => state);
+    const [isClient, setIsClient] = useState(false);
+
+    //After the component runs on the client side, the useEffect hook is used to update isClient to true.
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
+    if (!isClient) return (<aside className={cn("fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50" ) }> 
+    <ToggleSkeleton></ToggleSkeleton>
+    <RecommendedSkeleton></RecommendedSkeleton>
+    </aside>);
+    
     return ( 
         <aside className={cn("fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#2D2E35] z-50" ,collapsed && "w-[70px]") }>
             {children}
