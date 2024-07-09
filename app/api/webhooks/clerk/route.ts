7bcecbb6d272bb81f48105614da81093b4027c9e
milktea <yuribling123@gmail.com>
2024-07-08 22:27:24 -0700

    :rocket: Stream Model

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -57,6 +57,13 @@ export async function POST(req: Request) {
                 externalUserId: payload.data.id,
                 username: payload.data.username,
                 imageUrl: payload.data.image_url,
+                // stream
+                stream:{
+                    create:{
+                        name:`${payload.data.username}'s stream`,
+                    },
+                }
+
             }
         });
     }
